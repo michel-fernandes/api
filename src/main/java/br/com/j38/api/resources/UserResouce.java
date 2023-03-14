48e2374ccd81899541f0f222fde5377ad88ab58f
michel@j38.com.br <michel.fernandes@localiza.com>
2023-03-14 08:23:41 -0300

    retornando DTO para UserResource

--- src/main/java/br/com/j38/api/resources/UserResouce.java
+++ src/main/java/br/com/j38/api/resources/UserResouce.java
@@ -1,7 +1,8 @@
 package br.com.j38.api.resources;
 
-import br.com.j38.api.domain.User;
+import br.com.j38.api.domain.dto.UserDTO;
 import br.com.j38.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResouce {
     @Autowired
+    private ModelMapper mapper;
+    @Autowired
     private UserService userService;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 
 }
