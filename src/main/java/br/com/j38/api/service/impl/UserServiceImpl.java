2e24afc538236ae35d5d6fe42e66e6eae7dc7bc0
michel@j38.com.br <michel.fernandes@localiza.com>
2023-03-14 08:06:57 -0300

    tratamento de exceção para objeto não encontrado

--- src/main/java/br/com/j38/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/j38/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.j38.api.service.impl;
 import br.com.j38.api.domain.User;
 import br.com.j38.api.repositories.UserRepository;
 import br.com.j38.api.service.UserService;
+import br.com.j38.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> userOptional = userRepository.findById(id);
-        return userOptional.orElse(null);
+        return userOptional.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
