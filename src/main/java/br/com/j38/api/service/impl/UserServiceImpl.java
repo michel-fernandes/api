ca921dc31f126dc385ec275686f4a9ee9b749993
michel@j38.com.br <michel.fernandes@localiza.com>
2023-03-14 06:39:54 -0300

    classe userServiceImpl resource findById

---
+++ src/main/java/br/com/j38/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package br.com.j38.api.service.impl;
+
+import br.com.j38.api.domain.User;
+import br.com.j38.api.repositories.UserRepository;
+import br.com.j38.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User findById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userOptional.orElse(null);
+    }
+}
