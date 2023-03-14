85f32faf0109d6349c78f2ff630d0a0b65925b03
michel@j38.com.br <michel.fernandes@localiza.com>
2023-03-14 06:39:32 -0300

    classe userService resource findById

--- src/main/java/br/com/j38/api/resources/UserResouce.java
+++ src/main/java/br/com/j38/api/resources/UserResouce.java
@@ -1,6 +1,8 @@
 package br.com.j38.api.resources;
 
 import br.com.j38.api.domain.User;
+import br.com.j38.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResouce {
+    @Autowired
+    private UserService userService;
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Michel", "michel@j38.com.br", "unitTest"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 
 }
